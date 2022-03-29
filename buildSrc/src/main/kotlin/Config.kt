724f441fcc897c69f8b35966ccf340d62f5fe00b
Kolchurin Konstantin <theunrealkosyak@gmail.com>
2022-03-29 21:05:55 +0700

    fix for logo image

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val targetSdk = 31
 
     const val versionCode = 1
-    const val versionName = "0.7.0"
+    const val versionName = "0.8.0"
 
     const val applicationId = "app.thirtyninth.githubviewer"
 }
